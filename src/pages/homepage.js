d255494edd2e8ddd2cdf7186df199882d2489bda
Keith Winfield <keithwinfield@Keiths-Mac-Pro.rem.remichel.com>
2019-05-08 15:44:19 -0400

    changes to shop page header

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -42,19 +42,19 @@ export default function HomePage(props) {
               </div>
               <h1 className='text-center mt-5'>Specials</h1>
               <div className='container row mt-5 justify-content-between col-12 mx-auto'>
-                <div className="card col-3">
+                <div className="card col-md-3">
                   <img className="card-img-top" src={require('../images/samplesaleitem1.jpg')} alt="Card image cap" />
                   <div className="card-body">
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                   </div>
                 </div>
-                <div className="card col-3">
+                <div className="card col-md-3">
                   <img className="card-img-top" src={require('../images/samplesaleitem2.jpg')} alt="Card image cap" />
                   <div className="card-body">
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                   </div>
                 </div>
-                <div className="card col-3">
+                <div className="card col-md-3">
                   <img className="card-img-top" src={require('../images/samplesaleitem3.jpg')} alt="Card image cap" />
                   <div className="card-body">
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -63,10 +63,10 @@ export default function HomePage(props) {
               </div>
             </div>
             <div className="row text-center mt-5 justify-content-around container mx-auto">
-              <div className='col-4 mt-5'>
+              <div className='col-md-4 mt-5'>
                 <img height={600} src={require('../images/aboutusimage.jpg')} alt=""/>
               </div>
-              <div className='col-4'>
+              <div className='col-md-4'>
                 <h2 className='aboutus'>About Us</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut suscipit diam. Donec pulvinar neque vitae euismod imperdiet. Curabitur tempus hendrerit metus vitae lobortis. Ut mollis nunc neque, sit amet malesuada mauris malesuada vitae. Curabitur vitae eros lectus. Suspendisse potenti. Vivamus consectetur nibh a quam ultrices luctus. Suspendisse ac nibh sem.</p>
               </div>
--- src/pages/shoppage.js
+++ src/pages/shoppage.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'
 
 import ProductListing from '../features/product-listing'
 
 export default function ShopPage(props) {
   return <div>
+            <Helmet>
+                <title>Marcella | Lee - HomePage</title>
+                <meta name="description" content="Helmet application" />
+            </Helmet>
+            <div className='shopJumbotron m-0 p-0'>
+              <h1 className='shopTitle'>Shop Now</h1>
+            </div>
             <div className='container mt-5'>
               <ProductListing />
             </div>
